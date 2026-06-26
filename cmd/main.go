2fbe16e059db1effad48ce988e8793e2cad2381c
ihcsan <kamil.isan@gmail.com>
2026-06-26 09:48:11 +0700

    feat: setup postgresql docker compose and gorm database connection with auth integration

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,19 @@ import (
 	"log"
 	"os"
 
+	"customs-clearance-api/database"
 	"customs-clearance-api/routes"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8082"
 	}
 
-	router := routes.SetupRouter()
+	db := database.InitDB()
+
+	router := routes.SetupRouter(db)
 	if err := router.Run(":" + port); err != nil {
 		log.Fatalf("gagal menjalankan server: %v", err)
 	}
---
+++ database/postgres.go
@@ -0,0 +1,74 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"customs-clearance-api/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func InitDB() *gorm.DB {
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5435"
+	}
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		user = "postgres"
+	}
+	password := os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "postgres"
+	}
+	dbname := os.Getenv("DB_NAME")
+	if dbname == "" {
+		dbname = "customs_clearance"
+	}
+	sslmode := os.Getenv("DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Jakarta",
+		host, user, password, dbname, port, sslmode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("Gagal terhubung ke database: %v", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Gagal mengambil instance SQL DB: %v", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	log.Println("Koneksi database PostgreSQL berhasil diinisialisasi")
+
+	log.Println("Menjalankan AutoMigrate...")
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatalf("Gagal melakukan AutoMigrate: %v", err)
+	}
+	log.Println("AutoMigrate berhasil dijalankan")
+
+	DB = db
+	return db
+}
